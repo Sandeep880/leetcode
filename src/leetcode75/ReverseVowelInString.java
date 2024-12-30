3f8e6fb6cb643265c5af51e0b6721dd1aff77bd0
sandeep <asandeep2099@gmail.com>
2024-12-30 13:52:29 +0530

    leetcode-75

--- src/leetcode75/ReverseVowelInString.java
+++ src/leetcode75/ReverseVowelInString.java
@@ -10,9 +10,39 @@ public class ReverseVowelInString {
 	
     public static String reverseVowels(String s) {
 		char[] str= s.toCharArray();
+		int start=0,end = str.length-1;
+		while(end>= start)
+		{
+			if(isVowel(str[start]) && isVowel(str[end]))
+			{
+				Character temp = str[start];
+				str[start] = str[end];
+				str[end] = temp;
+				start++;
+				end--;
+			}
+			else if(isVowel(str[start]))
+				end--;
+			else
+				start++;
+			
+		}
+		String res="";
 		for(int i=0;i<str.length;i++)
-		  System.out.print(str[i] + " ");
-    	return "";
+			  res = res+ str[i];
+		
+    	return res;
+    }
+    
+    private static boolean isVowel(Character c) {
+    	if(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
+    				c == 'a' ||c == 'e' ||c == 'i' ||c == 'o' ||c == 'u' )
+    		{
+    			return true;
+    		}
+    	else
+    		return false;
+    	
     }
 
 }
