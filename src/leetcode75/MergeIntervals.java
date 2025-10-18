bed13498f7b512f1d6fbff2f6133507bdb640017
Sandeep880 <asandeep2099@gmail.com>
2025-10-18 12:39:24 +0530

    Leetcode 56

---
+++ src/leetcode75/MergeIntervals.java
@@ -0,0 +1,18 @@
+package leetcode75;
+
+public class MergeIntervals {
+
+	public static void main(String[] args) {
+
+		int intervals[][] = {{1,3},{2,6},{8,10},{15,18}};
+		
+	}
+	
+    public static int[][] merge(int[][] intervals) {
+    	
+    	int res[][] = new int[1][1];
+        
+    	return res;
+    	}    
+
+}
